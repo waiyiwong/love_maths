ed13c68843bbd7c44afa5bfe63a6749faff67f34
Wai Yi <wongwaverley@gmail.com>
2024-11-09 10:37:09 +0000

    add subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,8 +43,10 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
     }
-        else {
+    else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -86,9 +88,12 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"];
     }
     else {
-        alert("other game type not available");
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
     }
 
 }
@@ -131,4 +136,10 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+    document.getElementById("operand1").innerText = operand1 * operand2;
+    document.getElementById("operand2").innerText = operand2;
+    document.getElementById("operator").innerText = "/";
 }
